b7fd27a2fba5d2aed88325f59626f1a266efd31e
gustavomeloo <gugamelo2@gmail.com>
2022-10-20 17:24:43 -0300

    crud user feito e login

--- Api/Controllers/UserController.js
+++ Api/Controllers/UserController.js
@@ -1,4 +1,7 @@
+const User = require("../Models/User")
+const {hash, genSalt, compare} = require('bcryptjs')
 
+const jwt = require("jsonwebtoken")
 
 class UserController {
     static welcome = (req, res) => {
@@ -6,6 +9,112 @@ class UserController {
             message : "Bem vindo"
         })
     }
+
+
+    static login = async (req, res) => {
+        const {email, password} = req.body
+
+        const user = await this.findUser(email)
+
+        if(!user) {
+            res.status(422).json({
+                msg : 'usuário não encontrado'
+            })
+            return
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+            console.log("um erro")
+            return
+        }
+
+        try {
+            const secret = process.env.SECRET
+
+            const token = jwt.sign({id : user._id}, secret)
+
+            res.status(200).json({
+                msg : "autenticação realizada com sucesso",
+                token
+            })
+        } catch (error) {
+           // throw new Error("um erro aconteceu")
+        }
+
+    }
+
+    static createUser = async (req, res) => {
+        const {name, email, password} = req.body
+
+        const salt = await genSalt(12) 
+
+        const passwordHash = await hash(password, salt)
+
+        const user = new User({name, email, password : passwordHash})
+
+        try {
+            await user.save()
+
+            res.status(201).json({
+                msg : 'usuário criado com sucesso'
+            })
+        } catch (error) {
+            res.status(500).json({
+                msg : 'erro ao cadastrar'
+            })
+        }
+    }
+
+    static findUser = async (email) => {
+
+        try {
+
+            const user = await User.findOne({
+                email
+            })
+
+           return user
+
+        } catch (error) {
+           // throw new Error("um erro foi encontrado")
+        }
+    
+    }
+
+    static updateUser = async (req, res) => {
+
+        const id = req.params.id
+
+        const {name, email, password} = req.body
+
+        const salt = await genSalt(12) 
+
+        const passwordHash = await hash(password, salt)
+
+        const user = new User({name, email, password : passwordHash})
+
+        try {
+            const updateUser = await User.updateOne({
+                _id : id
+            }, {user})
+
+
+            if(updateUser.matchedCount == 0) {
+                res.status(422).json({
+                    msg : 'usuário não encontrado'
+                })
+
+                return
+            }
+
+            res.status(200).json(user)
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+
 }
 
 module.exports = UserController
--- Api/userRoute.js
+++ Api/userRoute.js
@@ -5,5 +5,8 @@ const UserController = require('./Controllers/UserController')
 const userRoute = express.Router()
 
 userRoute.get('/', UserController.welcome)
+userRoute.post('/user', UserController.createUser)
+userRoute.put('/user/:id', UserController.updateUser)
+userRoute.post('/login', UserController.login)
 
 module.exports = userRoute
